d581dde3527615e2a8311990d1203c6e4ceb3bd5
Attawit Kittikrairit <atton16@gmail.com>
2026-07-31 21:58:11 +0700

    refactor

---
+++ src/api/common/cmd.ts
@@ -0,0 +1,96 @@
+import { cp, chmod, chown } from "node:fs/promises";
+
+export const createCmds = (): Array<{
+  command?: string;
+  source?: string;
+  destination?: string;
+  path?: string;
+  uid?: number;
+  gid?: number;
+  mode?: number;
+  error?: any;
+}> => [];
+
+export const execCmds = async (
+  cmds: Array<{
+    command?: string;
+    source?: string;
+    destination?: string;
+    path?: string;
+    uid?: number;
+    gid?: number;
+    mode?: number;
+    error?: any;
+  }>,
+) => {
+  for (let i = 0; i < cmds.length; i++) {
+    const cmd = cmds[i];
+    if (cmd.command === "cp") {
+      try {
+        await cp(cmd.source!, cmd.destination!, {
+          recursive: true,
+          force: false,
+          errorOnExist: true,
+          preserveTimestamps: true,
+        });
+      } catch (error) {
+        console.error(error);
+        cmd.error = error;
+        break;
+      }
+    } else if (cmd.command === "chmod") {
+      try {
+        await chmod(cmd.path!, cmd.mode!);
+      } catch (error) {
+        console.error(error);
+        cmd.error = error;
+        break;
+      }
+    } else if (cmd.command === "chown") {
+      try {
+        await chown(cmd.path!, cmd.uid!, cmd.gid!);
+      } catch (error) {
+        console.error(error);
+        cmd.error = error;
+        break;
+      }
+    }
+  }
+  return cmds;
+};
+export const createCp = ({
+  source,
+  destination,
+}: {
+  source: string;
+  destination: string;
+}) => ({
+  command: "cp",
+  source,
+  destination,
+});
+export const createChmod = ({
+  path,
+  mode,
+}: {
+  path: string;
+  mode: number;
+}) => ({
+  command: "chmod",
+  path,
+  mode,
+});
+export const createChown = ({
+  path,
+  uid,
+  gid,
+}: {
+  path: string;
+  uid: number;
+  gid: number;
+}) => ({
+  command: "chown",
+  path,
+  uid,
+  gid,
+});
--- src/api/copy.ts
+++ src/api/copy.ts
@@ -1,5 +1,12 @@
 import { Hono } from "hono";
 import { stat, cp, chmod, chown } from "node:fs/promises";
+import {
+  createCmds,
+  execCmds,
+  createCp,
+  createChmod,
+  createChown,
+} from "./common/cmd";
 
 export const copy = new Hono();
 
@@ -8,16 +15,7 @@ copy.post("/", async (c) => {
   const json = await c.req.json();
   logger(json);
   const { sources, destination } = json;
-  const cmds: Array<{
-    command?: string;
-    source?: string;
-    destination?: string;
-    path?: string;
-    uid?: number;
-    gid?: number;
-    mode?: number;
-    error?: any;
-  }> = [];
+  const cmds = createCmds();
   for (let i = 0; i < sources.length; i++) {
     const source = sources[i];
     const parts = source.split("/");
@@ -26,60 +24,28 @@ copy.post("/", async (c) => {
     const uid = filestat.uid;
     const gid = filestat.gid;
     const mode = filestat.mode;
-    cmds.push({
-      command: "cp",
-      source,
-      destination: `${destination}/${filename}`,
-    });
-    cmds.push({
-      command: "chmod",
-      path: `${destination}/${filename}`,
-      mode,
-    });
-    cmds.push({
-      command: "chown",
-      path: `${destination}/${filename}`,
-      uid,
-      gid,
-    });
+    cmds.push(
+      createCp({
+        source,
+        destination: `${destination}/${filename}`,
+      }),
+    );
+    cmds.push(
+      createChmod({
+        path: `${destination}/${filename}`,
+        mode,
+      }),
+    );
+    cmds.push(
+      createChown({
+        path: `${destination}/${filename}`,
+        uid,
+        gid,
+      }),
+    );
   }
   logger(cmds);
-  for (let i = 0; i < cmds.length; i++) {
-    const cmd = cmds[i];
-    if (cmd.command === "cp") {
-      try {
-        await cp(cmd.source!, cmd.destination!, {
-          recursive: true,
-          force: false,
-          errorOnExist: true,
-          preserveTimestamps: true,
-        });
-      } catch (error) {
-        console.error(error);
-        logger(error);
-        cmd.error = error;
-        break;
-      }
-    } else if (cmd.command === "chmod") {
-      try {
-        await chmod(cmd.path!, cmd.mode!);
-      } catch (error) {
-        console.error(error);
-        logger(error);
-        cmd.error = error;
-        break;
-      }
-    } else if (cmd.command === "chown") {
-      try {
-        await chown(cmd.path!, cmd.uid!, cmd.gid!);
-      } catch (error) {
-        console.error(error);
-        logger(error);
-        cmd.error = error;
-        break;
-      }
-    }
-  }
+  await execCmds(cmds);
   return c.json({
     sources,
     destination,
